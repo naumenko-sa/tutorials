e4db15f1844ecd7c5d6d12590cc95583417ddf66
naumenko-sa <sergey_bulk@list.ru>
2016-11-29 11:19:23 -0500

    modified:   GEMINI-R.Rmd 	modified:   GEMINI-R.html 	new file:   NA12878-1-ensemble.db 	new file:   gemini_tutorial.R

---
+++ gemini_tutorial.R
@@ -0,0 +1,73 @@
+##########################################################
+##   Tutorial for CCM bioinformatics series, Sick Kids
+##########################################################
+
+setwd("~/Desktop/teaching_n_learning/tutorials/")
+
+library(RSQLite)
+library(plyr)
+
+dbname="NA12878-1-ensemble.db"
+
+con = dbConnect(RSQLite::SQLite(),dbname=dbname)
+
+dbListTables(con)
+
+qrySample="select name from samples"
+
+samples = dbGetQuery(con,qrySample)
+sample = samples[[1]]
+
+qryReport=paste0("select 
+        v.ref as Ref,
+        v.alt as Alt,
+        v.chrom as Chrom,
+        v.start+1  as Pos,
+        v.impact as Variation,
+        v.depth as Depth,
+        v.qual_depth as Qual_depth,
+        v.gene as Gene,
+        g.ensembl_gene_id as Ensembl_gene_id,
+        v.clinvar_disease_name as Clinvar,
+        v.transcript as Ensembl_transcript_id,
+        v.aa_length as AA_position,
+        v.exon as Exon,
+        v.pfam_domain as Pfam_domain,
+        v.rs_ids as rsIDs,
+        v.aaf_1kg_all as Maf_1000g,
+        v.aaf_exac_all as Exac_maf,
+        v.max_aaf_all as Maf_all,
+        v.exac_num_het as Exac_het,
+        v.exac_num_hom_alt as Exac_hom_alt,
+        v.sift_score as Sift_score,
+        v.polyphen_score as Polyphen_score,
+        v.cadd_scaled as Cadd_score
+        v.aa_change as AA_change
+        from variants v, gene_detailed g
+        where v.transcript=g.transcript and v.gene=g.gene");
+
+variants = dbGetQuery(con,qryReport)
+
+variants.rare.missense = subset(variants,Variation == 'missense_variant' & Maf_all < 0.01)
+
+variants.potentially_harmful = subset(variants.rare.missense, Polyphen_score > 0.9)
+
+#explore variation
+effects = count(variants,'Variation')
+op <- par(mar = c(15,4,4,2) + 0.1)
+barplot(effects$freq,names.arg = effects$Variation,las=2)
+par(op)
+
+#remove intronic variants
+effects.no_intronic = effects[effects$Variation != 'intron_variant',]
+op <- par(mar = c(15,4,4,2) + 0.1)
+barplot(effects.no_intronic$freq,names.arg = effects.no_intronic$Variation,las=2)
+par(op)
+
+qryGene_detailed = "select * from gene_detailed"
+gene_detailed = dbGetQuery(con,qryGenes)
+
+qryGene_summary = "select * from gene_summary"
+gene_summary = dbGetQuery(con,qryGene_summary)
+
+dbDisconnect(con)
