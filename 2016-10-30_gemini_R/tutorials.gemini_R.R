956c5953cab164a6e3e285e489154739560ce874
naumenko.sa <evolgenomicslab@gmail.com>
2019-03-06 16:49:41 -0500

    updated gemini_R tutorial

---
+++ 2016-10-30_gemini_R/tutorials.gemini_R.R
@@ -0,0 +1,106 @@
+###############################################################################
+##   Tutorial for CCM bioinformatics series, Sick Kids, 2016-10-30
+###############################################################################
+
+# installation
+# install.packages("RSQLite")
+# install.packages("tidyverse")
+
+setwd("~/Desktop/teaching_n_learning/tutorials/2016-10-30_gemini_R/")
+
+library(DBI)
+library(tidyverse)
+
+# download example database and additional files from:
+# https://drive.google.com/drive/folders/0B_bLL10GwDnsVWl6cUhQZVRUTVk
+
+dbname <- "gemini.db"
+con <- dbConnect(RSQLite::SQLite(), dbname = dbname)
+dbListTables(con)
+dbListFields(con, "variants")
+
+qrySample <- "select name from samples"
+
+samples <- dbGetQuery(con, qrySample)
+sample <- samples[[1]]
+
+qryReport <- "select 
+        v.ref as Ref,
+        v.alt as Alt,
+        v.chrom as Chrom,
+        v.start+1  as Pos,
+        v.impact as Variation,
+        v.depth as Depth,
+        v.qual_depth as Qual_depth,
+        v.gene as Gene,
+        g.ensembl_gene_id as Ensembl_gene_id,
+        v.clinvar_disease_name as Clinvar,
+        v.transcript as Ensembl_transcript_id,
+        v.aa_length as AA_position,
+        v.exon as Exon,
+        v.pfam_domain as Pfam_domain,
+        v.rs_ids as rsIDs,
+        v.aaf_1kg_all as Maf_1000g,
+        v.aaf_exac_all as Exac_maf,
+        v.max_aaf_all as Maf_all,
+        v.exac_num_het as Exac_het,
+        v.exac_num_hom_alt as Exac_hom_alt,
+        v.sift_score as Sift_score,
+        v.polyphen_score as Polyphen_score,
+        v.cadd_scaled as Cadd_score,
+        v.aa_change as AA_change,
+        from variants v, gene_detailed g
+        where v.transcript=g.transcript and v.gene=g.gene";
+
+variants <- as_tibble(dbGetQuery(con, qryReport))
+            
+variants_rare_missense <- filter(variants, Variation == "missense_variant",Maf_all < 0.01)
+variants_potentially_deleterious <- filter(variants_rare_missense, Polyphen_score > 0.9)
+
+#explore variation
+effects <- count(variants, "Variation")
+op <- par(mar = c(15,4,4,2) + 0.1)
+barplot(effects$freq, names.arg = effects$Variation, las=2)
+par(op)
+
+#remove intronic variants
+effects_no_intronic <- effects[effects$Variation != 'intron_variant',]
+op <- par(mar = c(15,4,4,2) + 0.1)
+barplot(effects_no_intronic$freq, names.arg = effects_no_intronic$Variation, las = 2)
+par(op)
+
+qryGene_detailed <- "select * from gene_detailed"
+gene_detailed <- dbGetQuery(con, qryGene_detailed)
+
+qryGene_summary <- "select * from gene_summary"
+gene_summary <- dbGetQuery(con, qryGene_summary)
+
+# additional information
+# gene descriptions
+gene_descriptions <- read_tsv("ensembl_w_description.txt")
+variants <- left_join(variants, gene_descriptions, by = c("Ensembl_gene_id" = "ensembl_gene_id"))
+
+# ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/functional_gene_constraint/README_fordist_cleaned_exac_r03_z_data_pLI_2016_01_13.txt
+exac_scores <- read_tsv("exac_scores.txt")
+variants <- left_join(variants, exac_scores, by = c("external_gene_name" = "gene"))
+
+qryAllTranscriptsAffected <- "select v.chrom, 
+                                v.start+1,
+                                v.end, 
+                                v.ref,
+                                v.alt,
+                                v.gene,
+                                vi.gene,
+                                v.transcript,
+                                vi.transcript,
+                                vi.aa_change,
+                                vi.aa_length 
+                            from 
+                                variant_impacts vi, 
+                                variants v 
+                            where 
+                                vi.variant_id = v.variant_id and
+                                v.is_coding = 1"
+all_coding_effects = dbGetQuery(con,qryAllTranscriptsAffected)
+
+dbDisconnect(con)
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# tutorials
-
-* [Exploring genome variation with GEMINI Framework in R ](https://github.com/naumenko-sa/tutorials/2016-10-30_gemini_R/blob/master/GEMINI-R.html): CCM Bioinformatics Tutorials, 2016-11-30, Toronto
+# tutorials I taught
+* [Exploring small variants with GEMINI database in R](https://docs.google.com/presentation/d/1DQ0dInX2VXSYE137nOzgeFKVOv5Ww5OBIENjBZRQDr0/edit?usp=sharing): CCM Bioinformatics Tutorials, 2016-11-30, Toronto
 * [Exploring genome variation and downstream analysis, RNA-seq data analysis](https://docs.google.com/presentation/d/1RRnTsTWYn-Yo5OahbFj4F4ViWPuHIvMkzfYrOqxutv0/edit?usp=sharing):CCM tutorial, 2017-04-28, Toronto
 * [Genomics in R: genes, regions, variants](https://docs.google.com/presentation/d/1T7cJzlQ5LwiOaK_19kSl3_67uYrNXy6XPsnTbdszE24/edit?usp=sharing): CCM tutorial, 2017-11-21, Toronto
 * [Genome analysis sequencing tutorial](https://docs.google.com/presentation/d/1DR4iIY59p40pk404gv-QZqUhg4hpinMdT_Ub9UMR7wg/edit?usp=sharing):CCM bioinformatics Tutorials, 2018-03-16, Toronto
 * [Variant calling and annotation tutorial](https://gist.github.com/naumenko-sa/82df1cb7d9b5f64691bf437f0eb455f0), [lecture slides](https://drive.google.com/open?id=158TJQTiluXCbTEcasNp0WfTylhykwtAk): Genomics in Medicine bioinformatics.ca workshop, module 3, 2018-06-13, Montreal
 * [Genomics in R: genes, regions, variants](https://docs.google.com/presentation/d/1q4B67xabkzEnSXK9wQKRYwiGeJgyHSjPTJqMN98CCcE/edit?usp=sharing): CCM tutorial, 2018-11-21, Toronto
 * [Genomics in R: genes, regions, variants (biomaRt, VariantAnnotation, TopMed)](https://docs.google.com/presentation/d/1rTG-FDQ9y8c8e6cO1WdVwq0_BF_R1VRrjTgUD1Vmv54/edit?usp=sharing): A Guest lecture for UofT Medical Genomics program, 2019-01-30, Toronto.
+
+# tutorials, courses, and books I found useful
+* [Applied Computational Genomics by Aaron Quinlan](https://github.com/quinlan-lab/applied-computational-genomics)
+* [HBC training tutorials](https://github.com/hbctraining)
+* [Data Visualization by Claus O. Wilke](https://serialmentor.com/dataviz/)
+* [Evolutionary Biology by AS Kondrashov](https://www.youtube.com/watch?v=fKv2UBkfmA0&list=PLfMMXtHYUY1r6ONv8pzOXCIt5BzH7gxPs)
