d195280e2b1d6ccff60749679bcbf28dae2cb6f5
naumenko.sa <evolgenomicslab@gmail.com>
2019-03-21 17:07:22 -0400

    cleaning

--- 2019-03-22_variant_prioritization/tutorials.prioritization.R
+++ 2019-03-22_variant_prioritization/tutorials.prioritization.R
@@ -1,6 +1,13 @@
 ###############################################################################
 ##   Tutorial for CCM bioinformatics series, The Hospital for Sick Children, 2019-03-20
 ###############################################################################
+# use select and then Ctrl + Enter to source a function
+filter_variants <- function(variants, proband, mom, dad){
+    variants_filtered <- filter(variants, Zygosity.Ashkenazim_HG002 == proband, 
+                                 Zygosity.Ashkenazim_HG003 == mom,
+                                 Zygosity.Ashkenazim_HG004 == dad)    
+    return(variants_filtered)
+}
 
 # installation
 # install.packages("tidyverse")
@@ -11,65 +18,107 @@ library(tidyverse)
 setwd("~/Desktop/teaching_n_learning/2019-03-22_variant_prioritization/")
 
 # download ashkenazim.small_variants.csv
-# https://drive.google.com/open?id=1AiiITUwjlYQIssTWRZ-DTVS3a0_PFKJO 
+# https://drive.google.com/open?id=15bYaktk50qD6qeL-LPgm5NSlk4-XFfYq
 
 library(googledrive)
-drive_find(n_max=50, type = "csv")
-#drive_download("~/tutorials/2019_03_variant_prioritization/ashkenazim.small_variants.csv")
-id <- "1AiiITUwjlYQIssTWRZ-DTVS3a0_PFKJO"
+drive_find(n_max = 50, type = "csv")
+# drive_download("~/tutorials/2019_03_variant_prioritization/ashkenazim.small_variants.csv")
+# assignment <- Alt -
+id <- "15bYaktk50qD6qeL-LPgm5NSlk4-XFfYq"
+# download by ID
 drive_download(as_id(id), overwrite = T)
-drive_download(as_id("https://drive.google.com/open?id=1AiiITUwjlYQIssTWRZ-DTVS3a0_PFKJO"), overwrite = T)
+# download by URL
+drive_download(as_id("https://drive.google.com/open?id=15bYaktk50qD6qeL-LPgm5NSlk4-XFfYq"), overwrite = T)
 
-variants <- read_csv("ashkenazim.small_variants.csv")
-            
+# some values are None in Quality and it reads as character string rather than numerical value
+variants <- read_csv("ashkenazim.small_variants.csv", na = c("None"))
+spec(variants)
+# remove low quality variants supported by 2 variant callers
+variants <- filter(variants, Number_of_callers > 2)
+
+# do it in excel first            
 variants_rare_missense <- filter(variants, Variation == "missense_variant", Maf_all < 0.01)
-
 variants_potentially_deleterious <- filter(variants_rare_missense, Polyphen_score > 0.9)
 
-#explore variation
-effects <- count(variants, "Variation")
-op <- par(mar = c(15,4,4,2) + 0.1)
-barplot(effects$freq, names.arg = effects$Variation, las=2)
-par(op)
+# explore variation
+# pipe = Ctrl + Shift + M
+variation <- variants %>% count(Variation)
+# equivalent expression
+# count(variants, Variation)
+ggplot(data = variants) + geom_bar(mapping = aes(x = Variation)) + coord_flip()
 
-#remove intronic variants
-effects_no_intronic <- effects[effects$Variation != 'intron_variant',]
-op <- par(mar = c(15,4,4,2) + 0.1)
-barplot(effects_no_intronic$freq, names.arg = effects_no_intronic$Variation, las = 2)
-par(op)
+variants_recessive <- filter(variants, Zygosity.Ashkenazim_HG002 == "Hom")
 
-qryGene_detailed <- "select * from gene_detailed"
-gene_detailed <- dbGetQuery(con, qryGene_detailed)
+variants_recessive <- filter(variants, Zygosity.Ashkenazim_HG002 == "Hom", 
+                                       Zygosity.Ashkenazim_HG003 == "Het",
+                                       Zygosity.Ashkenazim_HG004 == "Het",
+                                       Omim_gene_description != "0")
+write_excel_csv(variants_recessive, "variants_recessive.csv")
 
-qryGene_summary <- "select * from gene_summary"
-gene_summary <- dbGetQuery(con, qryGene_summary)
+# example of how to create and use a function
+# variants_recessive <- filter_variants(variants, "Hom", "Het", "Het") %>% 
+#    filter(Omim_gene_description != "0")
 
-# additional information
-# gene descriptions
-gene_descriptions <- read_tsv("ensembl_w_description.txt")
-variants <- left_join(variants, gene_descriptions, by = c("Ensembl_gene_id" = "ensembl_gene_id"))
+# X linked
+variants_x_linked <- filter_variants(variants, "Hom","-", "Het") %>% 
+    filter(grepl("X",Omim_inheritance))
 
-# ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/functional_gene_constraint/README_fordist_cleaned_exac_r03_z_data_pLI_2016_01_13.txt
-exac_scores <- read_tsv("exac_scores.txt")
-variants <- left_join(variants, exac_scores, by = c("external_gene_name" = "gene"))
+# variants reported in clinvar 
+clinvar <- variants %>% count(Clinvar)
+# exersize : plot a bar chart
 
-qryAllTranscriptsAffected <- "select v.chrom, 
-                                v.start+1,
-                                v.end, 
-                                v.ref,
-                                v.alt,
-                                v.gene,
-                                vi.gene,
-                                v.transcript,
-                                vi.transcript,
-                                vi.aa_change,
-                                vi.aa_length 
-                            from 
-                                variant_impacts vi, 
-                                variants v 
-                            where 
-                                vi.variant_id = v.variant_id and
-                                v.is_coding = 1"
-all_coding_effects = dbGetQuery(con,qryAllTranscriptsAffected)
+# pull only clinvar pathogenic variants
+variants_clinvar_pathogenic <- filter(variants, Clinvar == "pathogenic")
 
-dbDisconnect(con)
+# pull only variants with high CADD and polyphen scores
+variants_computational <- filter(variants, Cadd_score > 30, Polyphen_score > 0.95, 
+                        Zygosity.Ashkenazim_HG002 != "-", Omim_gene_description != "0")
+# none of them is homozygous!
+variants_computational <- filter(variants, Cadd_score > 30, Polyphen_score > 0.95, 
+                        Zygosity.Ashkenazim_HG002 != "-", Omim_gene_description != "0",
+                        Omim_inheritance = "AD")
+
+# de novo
+variants_de_novo <- filter_variants(variants, "Het", "-", "-")
+variants_de_novo <- filter_variants(variants, "Het", "-", "-") %>% filter(Quality > 100)
+# DNMT is a candate?
+# save de novos
+write_excel_csv(variants_de_novo, "variants_denovo.csv", append = F)
+
+# Exercise: are there some more de novo variants?
+variants_de_novo <- filter_variants(variants, "Hom", "Het", "-") %>% filter(Quality > 100)
+# variants_de_novo <- filter_variants(variants, "Hom", "-", "Het") %>% filter(Quality > 100)
+write_excel_csv(variants_de_novo, "variants_denovo.csv", append = T)
+
+# review the file - why so many de novo variants on chrX?
+# is proband a male? - search for Ashkenazim trio
+# (son - father - mother)
+variants_chrx <- filter(variants, grepl("chrX", Position), Number_of_callers > 2)
+# has a paralogue in chrY pseudoatosomal region
+# https://www.ncbi.nlm.nih.gov/gene/414
+variants_chrx <- filter(variants_chrx, Gene != "ARSD")
+# what is wrong with ESX1 gene?
+# low alt depth - false positive
+# Het in ASMTL? - pseudoatosomal region (<2.7M)
+# https://www.ncbi.nlm.nih.gov/grc/human
+variants_chrx <- filter(variants_chrx, Gene != "ESX1")
+# all these variants are not de novos!
+
+
+# select a subset of columns
+colnames(variants)
+variants_de_novo_report <- select(variants_de_novo, c("Position", "Ref", "Alt", "Gene", "Variation",
+                                                      "Gnomad_maf", "rsIDs"))
+# exercise : select columns you'd want to report
+
+# TCAG small variant report for WGS for NA12878
+# https://drive.google.com/open?id=1RATcIzqoptbyfSoJHTxAUoHT23wCovcz
+drive_download(as_id("https://drive.google.com/open?id=1RATcIzqoptbyfSoJHTxAUoHT23wCovcz"), overwrite = T)
+wgs_small_variants_tcag <- read_tsv("NA12878.hc.vqsr.vcf_ext.tsv.annovar.out_SUBSET_rev26.5.chr22.tsv")
+
+# pull variants matching HGMD database 
+variants_hgmd <- filter(wgs_small_variants_tcag, grepl("DM",HGMD_tag))
+
+# exercise: choose your strategy on how to extract pathogenic variants, filter + select with pipe %>% 
+
+ 
